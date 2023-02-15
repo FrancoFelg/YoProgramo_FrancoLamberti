85f7392872bad417176e9f1457f5e39103d32749
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2023-02-15 11:58:51 -0300

    JWT 001 (NOT CHECKED)
    Se ha comenzado la codificación de JWT.
    No se ha ejecutado la aplicación, no se sabe si funciona.
    Se han creado las entidades, repositorios y servicios de roles y usuarios.
    Se va a comenzar con la implementación de JWT

---
+++ YoProgramo_FrancoLamberti/BackEnd/mgb/src/main/java/com/portfolio/mgb/Security/Entity/Rol.java
@@ -0,0 +1,38 @@
+package com.portfolio.mgb.Security.Entity;
+
+import com.portfolio.mgb.Security.Enums.RolNombre;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    public Rol(){}
+
+    public Rol(int id, RolNombre rolNombre) {
+        this.id = id;
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+}
